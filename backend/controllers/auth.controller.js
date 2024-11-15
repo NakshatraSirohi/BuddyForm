5b2ddd90c2cfbfd0880298d2922dd391f04f8ad4
Neha <ojhaneha0028@gmail.com>
2024-11-15 09:46:30 +0530

    backend setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "You have hit the signup hit point",
+    });
+}
+export const login = async (req, res) => {
+    res.json({
+        data: "You have hit the login hit point",
+    });
+}
+export const logout = async (req, res) => {
+    res.json({
+        data: "You have hit the logout hit point",
+    });
+}
